47eb46f4e98cb7b4ec7c07318e414af50790df77
Dada-maira <dadavali246@gmail.com>
2023-01-14 20:28:02 +0530

    my first file

---
+++ local.tf
@@ -0,0 +1,30 @@
+resource "local_file" "edfg" {
+filename = "terraform_file"
+content = "hi i'm me"
+}
+
+resource "local_file" "fdgbdf" {
+filename = "terraform_file1"
+content = "hi i'meghana"
+}
+
+resource "local_file" "gvfrgr" {
+filename = "terraform_file3"
+content = "hi i'm meghana"
+}
+
+resource "local_file" "frgrgr" {
+filename = "terraform_file4"
+content = "hi i'm meghana"
+}
+
+resource "local_file" "abcfgf" {
+filename = "terraform_file5"
+content = "hi i'm meghana"
+}
+
+resource "local_file" "abcdthyyg" {
+filename = "terraform_file6"
+content = "hi i'm meghana"
+}
+
